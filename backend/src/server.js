90d8af8e5ffcf481f2e94dad38056c78136c9e4a
vannovich <awontuvannovich@gmail.com>
2025-11-22 06:25:42 -0800

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import routes from "./routes/index.routes.js";
 dotenv.config();
@@ -8,6 +9,21 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
+// Routes
 app.use("/api", routes);
+
+//Make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  const distPath = path.join(__dirname, "../frontend/dist");
+
+  app.use(express.static(distPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}
+
 
 app.listen(port, () => console.log(`Running on port ${port}`));
