7fbfcec97883f0375f0ae8e2063e2ba33d0dd82d
vannovich <awontuvannovich@gmail.com>
2025-11-23 17:43:33 -0800

    update user commit

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import User from "../model/User.js";
 import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -96,5 +97,29 @@ export const logout = (_, res) => {
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log(error.message);
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    if (!profilePic)
+      return res.status(400).json({ message: "Profile pic is required" });
+    const userId = req.user._id;
+    const uploadResponse = await cloudinary.uploader.upload(profilePic, {
+      folder: "profile_pics",
+    });
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in update profile: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { signin, signup, logout } from "../controller/auth.controller.js";
+import {
+  signin,
+  signup,
+  logout,
+  updateProfile,
+} from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", signin);
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req, res) =>
+  res.status(200).json(req.user)
+);
+
 export default router;
