dd6f7e08c14f786669aed12549e81fbc5e009e3c
vannovich <awontuvannovich@gmail.com>
2025-11-23 18:36:33 -0800

    rate limitting implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // Routes
